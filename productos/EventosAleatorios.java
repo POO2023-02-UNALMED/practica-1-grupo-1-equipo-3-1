20fbafda970b268d476095d005334493ae472f14
Lualmara-5 <luimartinezra@unal.edu.co>
2023-10-03 22:37:37 -0500

    Update EventosAleatorios.java

--- productos/EventosAleatorios.java
+++ productos/EventosAleatorios.java
@@ -1,5 +1,29 @@
 package productos;
 
 public class EventosAleatorios{
-  
+ 
+/*Los Enum clasificados en 2*/
+
+enum EventoPaquete {
+	derrumbe,
+	diluvio,
+	tráfico,
+	guerrilla,
+	llorona,
+	atentado,
+	atraco,
+	tu vehiculo ha sufrido un daño
+}
+
+enum EventoAnimal{
+	escape,
+	muerto
+}	
+
+	// Atributos de la clase
+
+	private int probabilidad;
+	private EventoPaquete evento1;
+	private EventoAnimal evento2;
+
 }

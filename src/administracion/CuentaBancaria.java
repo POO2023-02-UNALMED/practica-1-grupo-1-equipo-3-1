fb5fbe187a80589f1e611bc74314d6e8f9114817
kevinramos2 <keramosl@unal.edu.co>
2023-10-04 12:48:58 -0500

    Comentario

--- src/administracion/CuentaBancaria.java
+++ src/administracion/CuentaBancaria.java
@@ -1,5 +1,8 @@
 package administracion;
 
 public class CuentaBancaria{
-  
+  /* KEVIN en esta clase deben de ir dos métodos que necesito para la parte de sucursal:
+   * get.saldo() y descontarSaldo()
+   * revise las lineas 304-313 para que pueda ver el uso que les voy a dar
+   */
 }

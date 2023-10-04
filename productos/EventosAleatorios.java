709ab8c65f60b80d493b8ee308c5415bd2dc8e00
Lualmara-5 <luimartinezra@unal.edu.co>
2023-10-03 23:13:44 -0500

    Update EventosAleatorios.java

--- productos/EventosAleatorios.java
+++ productos/EventosAleatorios.java
@@ -1,29 +1,60 @@
 package productos;
 
-public class EventosAleatorios{
- 
-/*Los Enum clasificados en 2*/
-
-enum EventoPaquete {
-	derrumbe,
-	diluvio,
-	tráfico,
-	guerrilla,
-	llorona,
-	atentado,
-	atraco,
-	tu vehiculo ha sufrido un daño
+public class EventosAleatorios {
+	
 }
+    // Enum para EventoPaquete
+    enum EventoPaquete {
+    	
+        DERRUMBE(?),              // Probabilidad del ? (Puede sumar 100% pero deberiamos dejar un porcentaje en el que no pase nada)
+        DILUVIO(?),               // Probabilidad del 
+        TRAFICO(?),               // Probabilidad del 
+        GUERRILLA(?),              // Probabilidad del 
+        LLORONA(?),               // Probabilidad del 
+        ATENTADO(?),               // Probabilidad del 
+        ATRACO(?),                // Probabilidad del 
+        DAÑO_EN_VEHICULO(?);     // Probabilidad del 
 
-enum EventoAnimal{
-	escape,
-	muerto
-}	
+        private int probabilidad;
 
-	// Atributos de la clase
+        // Constructor personalizado para asignar la probabilidad
+        private EventoPaquete(int probabilidad) {
+            this.probabilidad = probabilidad;
+        }
 
-	private int probabilidad;
-	private EventoPaquete evento1;
-	private EventoAnimal evento2;
+        // Método para obtener la probabilidad
+        public int getProbabilidad() {
+            return probabilidad;
+        }
+    }
 
-}
+    // Enum para EventoAnimal
+    enum EventoAnimal {
+    	
+        ESCAPE(?),                // Probabilidad del ? (La probabilidad acá no debe sumar 100% no todos los casos escapa o muere) 
+        MUERTO(?);               // Probabilidad del 
+
+        private int probabilidad;
+
+        // Constructor personalizado para asignar la probabilidad
+        private EventoAnimal(int probabilidad) {
+            this.probabilidad = probabilidad;
+        }
+
+        // Método para obtener la probabilidad
+        public int getProbabilidad() {
+            return probabilidad;
+        }
+    }
+
+    // Atributos de la clase
+    private int probabilidad;
+    private EventoPaquete eventoPaquete;
+    private EventoAnimal eventoAnimal;
+
+    // Constructor con valor predeterminado para la probabilidad  (VIERNES HABLAMOS DE ESTO)
+    public EventosAleatorios() {
+        probabilidad = 0;  // Valor predeterminado de probabilidad
+        eventoPaquete = EventoPaquete.LLORONA; // Valor personalizado para eventoPaquete
+        eventoAnimal = EventoAnimal.MUERTO;    // Valor personalizado para eventoAnimal
+    }

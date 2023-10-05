a3989c3c5b1a1d78d64354ee737f31ea49b361bf
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-10-04 19:44:20 -0500

    Update Camion.java

--- src/transportes/Camion.java
+++ src/transportes/Camion.java
@@ -1,5 +1,27 @@
 package transportes;
 
 public class Camion extends Transporte {
-    
+    private static int cant_camiones;
+    private int matricula;
+
+    public Camion(String ciudad_registro, float capacidad_carga, String fabricante, int matricula){
+        super(ciudad_registro, capacidad_carga, fabricante, 20);
+        this.matricula = matricula;
+        Camion.cant_camiones++;
+    }
+
+    @Override
+    public void mantenimiento(){
+        this.setEstado(20);
+    }
+    public void recogerPaquete(){
+        //Crear guia
+
+    }
+    public static int getCant_camiones() {
+        return cant_camiones;
+    }
+
+
 }
+

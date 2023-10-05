e0867d45b003fa5dc237d929cd81fa1e99a69e9f
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-10-04 19:43:51 -0500

    Update Avion.java

--- src/transportes/Avion.java
+++ src/transportes/Avion.java
@@ -1,5 +1,39 @@
 package transportes;
 
 public class Avion extends Transporte {
-    
+    private static int cant_aviones = 0;
+    private int matricula;
+    private String ciudad_actual;
+
+    private Guia guia;
+    public Avion(String ciudad_registro, float capacidad_carga, int matricula, String fabricante){
+        super(ciudad_registro, capacidad_carga, fabricante, estado);
+        this.matricula = matricula;
+    }
+
+    @Override
+    public void mantenimiento(){
+        this.estado = 30;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public Guia getGuia() {
+        return guia;
+    }
+
+    public void setGuia(Guia guia) {
+        this.guia = guia;
+    }
+
+    public static int getCant_Aviones() {
+        return cant_aviones;
+    }
 }
+

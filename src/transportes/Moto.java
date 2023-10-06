5f9f9a1635a5830f1358c0c3434b52b46483b857
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-10-06 13:57:11 -0500

    Update Moto.java

--- src/transportes/Moto.java
+++ src/transportes/Moto.java
@@ -1,5 +1,24 @@
 package transportes;
 
 public class Moto extends Transporte {
-    
+    private static int cant_motos;
+    private int matricula;
+
+    public Moto(string ciudad_registro, float capacidad_carga, string fabricante, int matricula){
+        super(ciudad_registro, capacidad_carga, fabricante, 20);
+        this.matricula = matrícula;
+        Moto.cant_motos++;
+    }
+
+    @Override
+    public void mantenimiento(){
+        this.setEstado(20);
+    }
+    public void recogerPaquete(){
+        //crear guia
+
+    public static int getCant_motos(){
+        return cant_motos;
+    }
+    }
 }

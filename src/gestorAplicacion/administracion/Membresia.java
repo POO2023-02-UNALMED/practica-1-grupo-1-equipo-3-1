1ca7f83deaca521e6bf68e466a729190552dbe39
tmurilloa <141962307+tmurilloa@users.noreply.github.com>
2023-10-06 16:36:53 -0500

    update

--- src/gestorAplicacion/administracion/Membresia.java
+++ src/gestorAplicacion/administracion/Membresia.java
@@ -1,6 +1,10 @@
 //Cabecera Archivo:
 // 
 package administracion;
+
+import java.util.Random;
+
+
 //Cabecera Clase:
 // La finalidad de esta clase es saber el beneficio de entrega
 // y demas beneficios que incluye la membresia que cada cliente tiene.
@@ -11,11 +15,23 @@ public class Membresia{
 	
 	/*KEVIN. ¿Podriamos colocar el caso en el que el cliente no tenga membresía? 
 			leer el método calcularCantidadEscalas en la línea 322 de la clase sucursal donde dice default.*/
-	public enum tipo{
-		SILVER,
-		GOLD,
-		PLATINUM
-	};// Estos seran los tipos de membresia disponibles
+	enum tipo{
+		DEFAULT(50),
+		SILVER(40),
+		GOLD(30),
+		PLATINUM(20);
+		
+		private int probabilidad;
+		
+		private tipo(int probabilidad) {
+			this.probabilidad = probabilidad;
+			}
+		
+		public int getProbabilidad() {
+			return probabilidad;
+			}
+		}
+	
 
 	public Membresia() {
 	}
@@ -24,6 +40,22 @@ public class Membresia{
 		this.cliente = cliente;
 		//Aqui en cada uno de los constructores estaria  
 		// el codigo que haria que la membresia fuera aleatoria
+	}
+	
+	public crearMembresia() {
+        Random random = new Random();
+        int probabilidadTotal = 100;
+        int numeroAleatorio = random.nextInt(probabilidadTotal) + 1; // Genera un número entre 1 y 100
+
+        int acumulado = 0;
+        for (tipo membresia : tipo.values()) {
+            acumulado += membresia.getProbabilidad();
+            if (numeroAleatorio <= acumulado) {
+                return membresia;
+            }
+        }
+        return tipo.DEFAULT;
+     }
 	}
 	
 	public tipo getBeneficio() {

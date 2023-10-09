8e22fcb218d351899201c27a52eef46065de8efd
tmurilloa <tmurilloa@unal.edu.co>
2023-10-08 19:31:04 -0500

    update

---
+++ practicaGrupo3/src/basedatos/Serializador.java
@@ -0,0 +1,19 @@
+package basedatos;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+
+public class Serializador {
+	private static File rutaTemp = new File("src\\basedatos\\temp");
+	
+	/* 
+ 	Este metodo es el encargado de serializar las listas 
+	que estan creadas en la clases.
+	*/
+
+
+}

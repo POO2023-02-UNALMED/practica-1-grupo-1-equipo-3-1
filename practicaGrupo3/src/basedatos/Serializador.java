fcbdc0466fe65fd9e59f4912ff3812675d8fbdf3
tmurilloa <tmurilloa@unal.edu.co>
2023-10-12 22:47:27 -0500

    update

--- practicaGrupo3/src/basedatos/Serializador.java
+++ practicaGrupo3/src/basedatos/Serializador.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 
+import administracion.Sucursal;
+
 public class Serializador {
 	private static File rutaTemp = new File("src\\basedatos\\temp");
 	
@@ -14,6 +16,11 @@ public class Serializador {
  	Este metodo es el encargado de serializar las listas 
 	que estan creadas en la clases.
 	*/
-
+	private static void serializar(Sucursal scrsl) {
+		FileOutputStream fos;
+		ObjectOutputStream oos;
+		File[] docs = rutaTemp.listFiles();
+		PrintWriter pw;
+	}
 
 }
